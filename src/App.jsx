60e4ad69a00e9e640dc3001efd77958589d818c7
RedKatzen <gabrieldeavilaf@gmail.com.br>
2024-08-01 11:32:33 -0300

    Criação do Game e GameOver; estrutração de Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
-import './App.css'
-import StartScreen from './components/StartScreen'
+import './App.css';
+
+import { useState } from 'react';
+
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import StartScreen from './components/StartScreen';
+import { wordsList } from './data/words';
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = 
+      categories[Math.floor(Math.random()*Object.keys(categories).length)];
+    const word = 
+      words[category][Math.floor(Math.random()*words[category].length)];
+    return {word, category};
+  }
+  const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.split('');
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    setGameStage(stages[1].name);
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className='app'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
 }
